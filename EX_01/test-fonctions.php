ab6f5ce584b13be1adb85ef297b6ce2c35f3e708
PerrineMetz <67265994+PerrineMetz@users.noreply.github.com>
2020-06-30 16:47:02 +0200

    day 6

--- EX_01/test-fonctions.php
+++ EX_01/test-fonctions.php
@@ -4,8 +4,23 @@
  $prix = "10.50"; 
  $disponible = "true";
  $quantite = "10";
+ $achat=5;
+ $nb_ajout=5;
  include ("affichage.php");
  include ("gestion-produit.php");
+ include ("chaine.php");
+ 
+afficher_produit($nom_produit,$couleur,$prix,$disponible);
+$quantite = achat($quantite,$achat);
+$disponible=update_dispo($quantite);
+
+afficher_produit($nom_produit,$couleur,$prix,$disponible);
+$quantite = achat($quantite,$achat);
+$disponible=update_dispo($quantite);
+ 
+afficher_produit($nom_produit,$couleur,$prix,$disponible);
+$quantite = restockage($quantite, $nb_ajout);
+$disponible=update_dispo($quantite);
  
 afficher_produit($nom_produit,$couleur,$prix,$disponible);
 ?>
